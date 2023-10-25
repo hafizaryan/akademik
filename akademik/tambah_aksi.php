77175c2c0bcd502a8fc836511ae45eee2364d804
Hafiz Aryan Siregar <117965934+hafizaryan@users.noreply.github.com>
2023-10-25 14:55:34 +0700

    Add files via upload

---
+++ akademik/tambah_aksi.php
@@ -0,0 +1,13 @@
+<?php 
+// koneksi database
+include 'koneksi.php';
+// menyimpan sementara data yang di kirim dari form ke variabel
+$nama = $_POST['nama'];
+$nim = $_POST['nim'];
+$alamat = $_POST['alamat'];
+// menginput data ke database
+mysqli_query($koneksi,"insert into mahasiswa 
+values('','$nama','$nim','$alamat')");
+// mengalihkan halaman kembali ke index.php
+header("location:index.php");
+?>
